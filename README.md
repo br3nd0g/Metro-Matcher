676df76b9412e686ee6e5430de6601cae3f53536
brendan :) <brendan.j.corcoran@gmail.com>
2022-10-05 13:04:11 +0100

    added safe url routing

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Metro-Matcher
 
-A web game made for my Advanced Higher CompSci Project
+A web game made for my Advanced Higher CompSci Project. 
+
+It consists of FILL IN WHEN DONE
--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
-app = Flask("True Spotify Shuffle")
+app = Flask("Metro Matcher")
 app.static_folder = 'static'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-app.run(host='0.0.0.0', port=81)
+@app.route('/leaderboard')
+def leaderboard():
+    return render_template('index.html')
+
+@app.route('/game')
+def game():
+    city = request.args.get('city')
+    if city == None: return redirect(url_for("index"))
+
+    return render_template('index.html')
+
+
+app.run(host='0.0.0.0', port=81, debug=True)
