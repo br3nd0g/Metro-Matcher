0b075ca5e2cfb4385efe299e0a0ccaecb12c6e10
Brendan Corcoran <96981774+br3nd0g@users.noreply.github.com>
2023-03-03 11:15:08 +0000

    for reasons unknown...

--- static/js/showLines.js
+++ static/js/showLines.js
@@ -1,3 +1,19 @@
+const coloursArea = document.getElementById("colours")
+const linesArea = document.getElementById("linesInfo")
+
 jsonString = linesFromFlask;
 const linesObj = JSON.parse(jsonString)
-alert(linesObj)
+console.log(linesObj)
+
+for(let line of linesObj){
+    let colourHtml = `<div style="background-color: ${line.colour}"></div>`
+
+    coloursArea.insertAdjacentHTML('afterbegin', colourHtml)
+
+    let lineHtml = `<div class="metroLine">
+        <h4 class="metroName">${line.name}</h4>
+        <div class="actualLine" style="background-color: ${line.colour}"></div>
+    </div>`
+
+    linesArea.insertAdjacentHTML('beforeend', lineHtml)
+}
