4d850146102bf67401ec756d36dee4a72cabfcf2
Brendan Corcoran <96981774+br3nd0g@users.noreply.github.com>
2023-03-03 18:36:58 +0000

    dfs

--- static/js/showLines.js
+++ static/js/showLines.js
@@ -12,8 +12,18 @@ for(let line of linesObj){
 
     let lineHtml = `<div class="metroLine">
         <h4 class="metroName">${line.name}</h4>
-        <div class="actualLine" style="background-color: ${line.colour}"></div>
-    </div>`
+        <div class="actualLine" style="background-color: ${line.colour}">`
+
+    for(let stop in line.stops){
+
+        let stopHtml = `<div style="background: linear-gradient(to bottom right, red, blue);" class="lineStop"></div>`
+
+        lineHtml += stopHtml
+
+
+    }
+        
+    lineHtml += '</div></div>'
 
     linesArea.insertAdjacentHTML('beforeend', lineHtml)
 }
